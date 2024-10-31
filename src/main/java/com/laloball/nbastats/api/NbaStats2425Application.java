8685b51f2ad06a58e5ddf6a9dd07491b0b7b871a
Eduardo.Valadez <eduardo.valadez197@hotmail.com>
2024-10-30 22:48:34 -0600

    Proyecto API Inicilizado

---
+++ src/main/java/com/laloball/nbastats/api/NbaStats2425Application.java
@@ -0,0 +1,13 @@
+package com.laloball.nbastats.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaStats2425Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaStats2425Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/laloball/nbastats/api/NbaStats2425ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laloball.nbastats.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaStats2425ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
